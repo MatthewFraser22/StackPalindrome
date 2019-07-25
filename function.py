0eb561c4a43461ae33b220e59df3141d4d5333dd
Matthew Fraser <52222907+MatthewFraser22@users.noreply.github.com>
2019-07-24 21:26:24 -0400

    Add files via upload

---
+++ function.py
@@ -0,0 +1,25 @@
+from Stack import Stack
+
+def palindrome(n):
+    palindrome = True
+    abc = 'abcdefghijklmnopqrstuvwxyz'
+    n = n.lower()
+    nospace = ''
+    s = Stack()
+    loc = 0
+     
+    for i in n:
+        if i in abc:
+            nospace += i
+    for i in nospace:
+        while loc < len(nospace)//2:
+            s.push(nospace[loc])
+            loc += 1
+             
+        while loc < len(nospace) and palindrome:
+            if s.pop() != nospace[loc]:
+                palindrome = False
+            loc += 1
+         
+             
+    return palindrome
